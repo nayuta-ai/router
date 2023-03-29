e81f0136099c1e1072588a98139d5d16797c1970
nayuta-ai <nayuta723@gmail.com>
2023-03-29 20:16:28 +0900

    Update utils_test and add arp_reply_arrives and arp_request_arrives test

--- test/arp_test.cpp
+++ test/arp_test.cpp
@@ -1,30 +1,49 @@
 #include "arp.h"
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#include "ethernet.h"
+#include "ip.h"
+#include "log.h"
+#include "my_buf.h"
 #include "net.h"
+#include "utils.h"
 
+// Declare the external array in the test file
+extern arp_table_entry arp_table[ARP_TABLE_SIZE];
+
+// Define the test fixture
 class ArpTableTest : public ::testing::Test {
  protected:
-  virtual void SetUp() { arp_table_entry arp_table[ARP_TABLE_SIZE]; }
+  // Initialize the arp_table array before each test
+  virtual void SetUp() {}
 
-  virtual void TearDown() {}
+  // Tear down the arp_table array after each test
+  virtual void TearDown() {
+    // Reset the arp_table to its initial state
+    arp_table_entry default_entry = {};
+    for (int i = 0; i < ARP_TABLE_SIZE; ++i) {
+      arp_table[i] = default_entry;
+    }
+  }
 };
 
-TEST(AddArpTableEntryTest, ValidCase) {
+TEST_F(ArpTableTest, AddValidCase) {
   net_device dev;
   uint8_t mac_addr[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06};
   uint32_t ip_addr = 0x0a000001;  // 10.0.0.1
+  const uint32_t index = ip_addr % ARP_TABLE_SIZE;
+
   add_arp_table_entry(&dev, mac_addr, ip_addr);
 
-  const uint32_t index = ip_addr % ARP_TABLE_SIZE;
   // Check that the entry was added to the correct location in the ARP table
   EXPECT_EQ(memcmp(arp_table[index].mac_addr, mac_addr, 6), 0);
   EXPECT_EQ(arp_table[index].ip_addr, ip_addr);
   EXPECT_EQ(arp_table[index].dev, &dev);
 }
 
-TEST(AddArpTableEntryTest, DuplicateCase) {
+TEST_F(ArpTableTest, AddDuplicateCase) {
   net_device dev1;
   uint8_t mac_addr1[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06};
   uint32_t ip_addr1 = 0x0a000001;  // 10.0.0.1
@@ -56,4 +75,127 @@ TEST(AddArpTableEntryTest, DuplicateCase) {
     }
   }
   EXPECT_EQ(cnt, 3);
+}
+
+TEST_F(ArpTableTest, SearchValidCase) {
+  net_device dev;
+  arp_table_entry *arp;
+  uint8_t mac_addr[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06};
+  uint32_t ip_addr = 0x0a000001;  // 10.0.0.1
+  add_arp_table_entry(&dev, mac_addr, ip_addr);
+
+  arp = search_arp_table_entry(ip_addr);
+
+  EXPECT_EQ(ip_addr, arp->ip_addr);
+  EXPECT_EQ(0, memcmp(arp->mac_addr, mac_addr, 6));
+}
+
+TEST_F(ArpTableTest, SearchNullCase) {
+  arp_table_entry *arp;
+  uint32_t ip_addr = 0x0a000001;  // 10.0.0.1
+
+  arp = search_arp_table_entry(ip_addr);
+
+  EXPECT_EQ(nullptr, arp);
+}
+
+TEST_F(ArpTableTest, DumpValidCase) {
+  net_device dev;
+  strcpy(dev.name, "dev");
+  arp_table_entry *arp;
+  uint8_t mac_addr[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06};
+  uint32_t ip_addr = 0x0a000001;  // 10.0.0.1
+  add_arp_table_entry(&dev, mac_addr, ip_addr);
+
+  testing::internal::CaptureStdout();
+  dump_arp_table_entry();
+  std::string output = testing::internal::GetCapturedStdout();
+
+  EXPECT_EQ(
+      "|---IP ADDRESS----|----MAC ADDRESS----|------DEVICE-------|-INDEX-|\n"
+      "|        10.0.0.1 | 01:02:03:04:05:06 |               dev |  0051 |\n"
+      "|-----------------|-------------------|-------------------|-------|"
+      "\n",
+      output);
+}
+
+TEST_F(ArpTableTest, ReplyArrivesTest) {
+  auto *dev =
+      (net_device *)calloc(1, sizeof(net_device) + sizeof(net_device_data));
+  uint8_t buffer[sizeof(arp_ip_to_ethernet)] = {0};
+  auto *eth = reinterpret_cast<arp_ip_to_ethernet *>(buffer);
+  arp_table_entry *arp;
+  ip_device ip_dev = {0x0302010a, 0, 0};
+  strcpy(dev->name, "dev");
+  dev->ip_dev = &ip_dev;
+  uint32_t ip_addr = 0x0101a8c0;
+  uint8_t sha[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06};
+  memccpy(eth->sha, sha, '\0', ETHERNET_ADDRESS_LEN);
+  eth->spa = ip_addr;
+
+  arp_reply_arrives(dev, eth);
+
+  // Check that the entry was added to the correct location in the ARP table
+  arp = search_arp_table_entry(ntohl(ip_addr));
+  EXPECT_TRUE(arp != nullptr);
+  EXPECT_EQ(ntohl(ip_addr), arp->ip_addr);
+  EXPECT_EQ(0, memcmp(arp->mac_addr, sha, 6));
+}
+
+void ethernet_encapsulate_output(net_device *dev, const uint8_t *dest_addr,
+                                 my_buf *payload_mybuf, uint16_t ether_type);
+
+class MockEthernet {
+ public:
+  MOCK_METHOD(void, ethernet_encapsulate_output,
+              (net_device * dev, const uint8_t *dest_addr,
+               my_buf *payload_mybuf, uint16_t ether_type));
+} *mocketh;
+
+void ethernet_encapsulate_output(net_device *dev, const uint8_t *dest_addr,
+                                 my_buf *payload_mybuf, uint16_t ether_type) {
+  mocketh->ethernet_encapsulate_output(dev, dest_addr, payload_mybuf,
+                                       ether_type);
+}
+
+class MockArpTest : public ArpTableTest {
+ protected:
+  void SetUp() override { mocketh = new MockEthernet(); }
+
+  void TearDown() override { delete mocketh; }
+};
+
+using ::testing::_;
+using ::testing::Invoke;
+
+TEST_F(MockArpTest, Request) {
+  auto *dev =
+      (net_device *)calloc(1, sizeof(net_device) + sizeof(net_device_data));
+  uint8_t buffer[sizeof(arp_ip_to_ethernet)] = {0};
+  auto *eth = reinterpret_cast<arp_ip_to_ethernet *>(buffer);
+  arp_table_entry *arp;
+  ip_device ip_dev = {0xc0a80101, 0, 0};
+  strcpy(dev->name, "dev");
+  dev->ip_dev = &ip_dev;
+  uint32_t target_addr = 0x0101a8c0;
+  uint32_t source_addr = 0x0201a8c0;
+  uint8_t tha[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06};
+  uint8_t sha[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x07};
+  memccpy(eth->tha, tha, '\0', ETHERNET_ADDRESS_LEN);
+  memccpy(eth->sha, sha, '\0', ETHERNET_ADDRESS_LEN);
+  eth->tpa = target_addr;
+  eth->spa = source_addr;
+  my_buf *mybuf = my_buf::create(ETHERNET_ADDRESS_LEN);
+  EXPECT_EQ(dev->ip_dev->address, ntohl(eth->tpa));
+
+  // expect send function to be called with correct arguments
+  EXPECT_CALL(*mocketh, ethernet_encapsulate_output(_, _, _, _));
+
+  // call the function under test
+  arp_request_arrives(dev, eth);
+
+  arp = search_arp_table_entry(ntohl(source_addr));
+  EXPECT_TRUE(arp != nullptr);
+  EXPECT_EQ(ntohl(source_addr), arp->ip_addr);
+  EXPECT_EQ(0, memcmp(arp->mac_addr, sha, 6));
 }
--- test/utils_test.cpp
+++ test/utils_test.cpp
@@ -29,7 +29,7 @@ TEST(HtonlTest, ValidTestCase) {
 TEST(MacAddrToaTest, ConvertsMacAddrToString) {
   uint8_t mac_addr[] = {0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC};
   const char* result_string = mac_addr_toa(mac_addr);
-  ASSERT_STREQ(result_string, "12:34:56:78:9a:bc");
+  EXPECT_STREQ("12:34:56:78:9a:bc", result_string);
 }
 
 TEST(IPNtoaTest, ConvertsIpAddressToString) {
@@ -48,12 +48,12 @@ TEST(IPNtoaTest, ConvertsIpAddressToString) {
 
 TEST(IPHtoaTest, ConvertsIpAddressToString) {
   // Test 1: Valid input
-  uint32_t ip = 0x0302010a;  // 3.2.1.10
-  EXPECT_STREQ("3.2.1.10", ip_htoa(ip));
+  uint32_t ip = 0x0a010203;  // 10.1.2.3
+  EXPECT_STREQ("10.1.2.3", ip_htoa(ip));
 
   // Test 2: Another valid input
-  ip = 0x0101a8c0;  // 1.1.168.192
-  EXPECT_STREQ("1.1.168.192", ip_htoa(ip));
+  ip = 0xc0a80101;  // 192.168.1.1
+  EXPECT_STREQ("192.168.1.1", ip_htoa(ip));
 
   // Test 3: Invalid input (0xffffffff)
   ip = 0xffffffff;
@@ -62,16 +62,9 @@ TEST(IPHtoaTest, ConvertsIpAddressToString) {
 
 TEST(ChecksumTest, BasicChecksumTest) {
   // Set up input data (a buffer of 4 bytes)
-  uint16_t buffer[2] = {0x1234, 0x5678};
+  uint16_t buffer[4] = {0x0000, 0x0000, 0x0000, 0x0000};
 
   // Call the function and check the result
-  uint16_t checksum = checksum_16(buffer, 4, 0);
-  EXPECT_EQ(checksum, 0x9753);
-
-  // Set up input data (a buffer of 5 bytes)
-  uint16_t buffer2[3] = {0x1234};
-
-  // Call the function and check the result
-  checksum = checksum_16(buffer2, 2, 0);
-  EXPECT_EQ(checksum, 0xedcb);
+  uint16_t checksum = checksum_16(buffer, 8, 0);
+  EXPECT_EQ(checksum, 0xffff);
 }
